ae99b2aab4865eec94c3bdfb622fd77f5c9f699c
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2023-04-02 13:44:45 +0545

    added landing page setup using React and React Router

---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+//functional component that will render the landing page
+function Landing() {
+  return (
+    <div>
+      <h1>Welcome to our Election App!</h1>
+      <p>
+        This is a decentralized voting platform built on Ethereum blockchain.
+      </p>
+    </div>
+  );
+}
+
+export default Landing;
