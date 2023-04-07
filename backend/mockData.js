2d8fdc12f7ad659355b840a36b8a83c5d3cf7e0e
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2023-04-07 08:40:41 +0545

    adding a mockdata for verification of voter's validity locally for testing purposes

---
+++ backend/mockData.js
@@ -0,0 +1,103 @@
+const mockData = [
+  {
+    name: "Dilip Poudel",
+    dob: "1990-01-01",
+    passport: "N0000001",
+  },
+  {
+    name: "Kamala Rai",
+    dob: "1991-02-02",
+    passport: "N0000002",
+  },
+  {
+    name: "Bikash Thapa",
+    dob: "1985-03-03",
+    passport: "N0000003",
+  },
+  {
+    name: "Laxmi Tamang",
+    dob: "1986-04-04",
+    passport: "N0000004",
+  },
+  {
+    name: "Ram Prasad Sharma",
+    dob: "1992-05-05",
+    passport: "N0000005",
+  },
+  {
+    name: "Sita Gurung",
+    dob: "1993-06-06",
+    passport: "N0000006",
+  },
+  {
+    name: "Narayan Bhattarai",
+    dob: "1980-07-07",
+    passport: "N0000007",
+  },
+  {
+    name: "Sabita Karki",
+    dob: "1981-08-08",
+    passport: "N0000008",
+  },
+  {
+    name: "Ramesh Singh",
+    dob: "1982-09-09",
+    passport: "N0000009",
+  },
+  {
+    name: "Anita Basnet",
+    dob: "1983-10-10",
+    passport: "N0000010",
+  },
+  {
+    name: "Dinesh Gurung",
+    dob: "1984-11-11",
+    passport: "N0000011",
+  },
+  {
+    name: "Sujata Tamang",
+    dob: "1994-12-12",
+    passport: "N0000012",
+  },
+  {
+    name: "Bimala Rai",
+    dob: "1985-01-13",
+    passport: "N0000013",
+  },
+  {
+    name: "Gopal Shrestha",
+    dob: "1986-02-14",
+    passport: "N0000014",
+  },
+  {
+    name: "Radhika Thapa",
+    dob: "1987-03-15",
+    passport: "N0000015",
+  },
+  {
+    name: "Shyam Prasad Koirala",
+    dob: "1988-04-16",
+    passport: "N0000016",
+  },
+  {
+    name: "Asha Tamang",
+    dob: "1989-05-17",
+    passport: "N0000017",
+  },
+  {
+    name: "Krishna Gurung",
+    dob: "1990-06-18",
+    passport: "N0000018",
+  },
+  {
+    name: "Nabin Bhandari",
+    dob: "1991-07-19",
+    passport: "N0000019",
+  },
+  {
+    name: "Anjali Rai",
+    dob: "1992-08-20",
+    passport: "N0000020",
+  },
+];
+module.exports = mockData;
