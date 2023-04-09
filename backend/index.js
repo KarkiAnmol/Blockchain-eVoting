345e21afec7f740a127ccbba8f26fd00f11f45fa
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2023-04-09 10:55:45 +0545

    updating the index.js for the updated route

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,35 @@
-const express =require("express");
-const app=express();
-const mongoose=require("mongoose")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const voterRoute=require('./routes/user')
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Database connection
 mongoose
-  .connect(process.env.mongodb_url)
+  .connect(process.env.mongodb_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
   });
-  app.use('/api/auth',voterRoute)
-  app.listen(process.env.PORT || 5000, () => {
-    console.log("Backend server is running!");
-  });
+
+//checking request
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
+//Routes
+const voterRoute = require("./routes/voterRoutes");
+app.use("/", voterRoute);
+
+//Start the server
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Backend server is running! at port ${process.env.PORT}`);
+});
