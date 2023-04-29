ce60fcd1abe85a634bdb98d945c22fb65b684e6e
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2023-04-29 17:15:50 +0545

    feat: create VoterList component with VoterCard and context API
    This commit adds the VoterList component with useContext hook and fetches data from the VotingProvider context. It also includes the VoterCard component to render the list of voters.

--- frontend/src/pages/VoterList.js
+++ frontend/src/pages/VoterList.js
@@ -1,7 +1,28 @@
-import React from "react";
+// Import necessary modules
+import React, { useState, useEffect, useContext } from "react";
+import { VotingProvider } from "../context/Voter";
+import { VotingContext, fetchContract } from "../context/Voter";
+import VoterCard from "../components/VoterCard";
+import "./VoterList.css";
 
+// Define the VoterList component
 const VoterList = () => {
-  return <div>VoterList</div>;
+  // Get the getAllVoterData function and voterArray from the VotingContext
+  const { getAllVoterData, voterArray } = useContext(VotingContext);
+
+  // Call getAllVoterData when the component mounts
+  useEffect(() => {
+    getAllVoterData();
+  }, []);
+
+  // Render the VoterList component
+
+  return (
+    <div className="voterList">
+      <VoterCard voterArray={voterArray} />
+    </div>
+  );
 };
 
+// Export the VoterList component
 export default VoterList;
