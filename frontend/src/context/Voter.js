a2bf979b07fd5f69fc68d0c9bbd294847d2bb235
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2023-04-29 10:29:22 +0545

    feat: Add VotingProvider and VotingContext with various state variables
    This commit adds a new component called VotingProvider with a VotingContext that wraps around child components. It provides various state variables such as currentAccount, candidateArray, voterArray, voterLength, etc. and functions such as checkIfWalletIsConnected, connectWallet, createVoter, getAllVoterData, etc. that are used throughout the application for fetching data and interacting with the smart contract. This commit also imports necessary dependencies such as Web3Modal, ethers, ipfs-http-client, etc. and exports a function called fetchContract that fetches the smart contract and allows us to communicate with it.

--- frontend/src/context/Voter.js
+++ frontend/src/context/Voter.js
@@ -1,24 +1,39 @@
 import React, { useState, useEffect, createContext, useContext } from "react";
-import { Web3Modal } from "@web3modal/react";
+import Web3Modal from "web3modal";
 import { ethers } from "ethers";
 import { create as ipfsHttpClient } from "ipfs-http-client";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Buffer } from "buffer";
 
 //internal imports
-import { votingAddress, votingAddressABI } from "./constants";
+import { VotingAddress, VotingAddressABI } from "./constants";
 import { seconds } from "@nomicfoundation/hardhat-network-helpers/dist/src/helpers/time/duration";
 
-const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+// const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+const projectId = "2OoInHAMMFhyet8kCoaOnEwyUeY";
+const projectSecret = "75229b2d8533c478eb558850795db1aa";
+const auth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString(
+  "base64"
+)}`;
 
+const client = ipfsHttpClient({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+  headers: {
+    authorization: auth,
+  },
+});
 //function to fetch the contract and allow us to communicate
 export const fetchContract = (signerOrProvider) =>
-  new ethers.Contract(votingAddress, votingAddressABI, signerOrProvider);
+  new ethers.Contract(VotingAddress, VotingAddressABI, signerOrProvider);
 
 export const VotingContext = createContext();
 
 export const VotingProvider = ({ children }) => {
   const votingTitle = "my first dapp";
+
   const [currentAccount, setCurrentAccount] = useState("");
   const [candidateLength, setCandidateLength] = useState("");
   const pushCandidate = [];
@@ -39,7 +54,7 @@ export const VotingProvider = ({ children }) => {
   const checkIfWalletIsConnected = async () => {
     if (!window.ethereum) return setError("Please Install Metamask");
     const account = await window.ethereum.request({
-      method: "ethers_accounts",
+      method: "eth_accounts",
     });
 
     if (account.length) {
@@ -60,14 +75,128 @@ export const VotingProvider = ({ children }) => {
 
   //upload to ipfs voter Image
   const uploadToIPFS = async (file) => {
+    const subdomain = "https://elect.infura-ipfs.io";
     try {
       const added = await client.add({ content: file });
-      const url = `http://ipfs.infura.io/ipfs/${added.path}`;
-      return url;
+      // console.log("added.path:", added.path);
+      const URL = `${subdomain}/ipfs/${added.path}`;
+      return URL;
     } catch (error) {
-      setError("Error uplloading to IPFS");
+      console.log("Error uploading file to IPFS.", error);
     }
   };
+  // const navigate = useNavigate();
+
+  //CREATE VOTER
+  const createVoter = async (formInput, fileUrl) => {
+    try {
+      const { name, address, position } = formInput;
+
+      console.log("button return:", name, address, position, fileUrl);
+
+      //Connecting smart contract
+      const providerOptions = {};
+      const web3Modal = new Web3Modal({
+        providerOptions, // required
+      });
+      const connection = await web3Modal.connect();
+      // console.log(web3Modal);
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract = fetchContract(signer);
+      // console.log(" create voter contract:", contract);
+
+      //UPLOAD VOTER INFO TO IPFS
+      const data = JSON.stringify({ name, address, position, image: fileUrl });
+      const added = await client.add(data);
+      const url = `https://infura-ipfs.io/ipfs/${added.path}`;
+      // console.log("VOTER DETAILS IN IPFS URL:", url);
+
+      // console.log("abpout ot use voter right function");
+      //use VOTERRIGHT FUNCTION SMART CONTRACT FROM FRONTEND
+      try {
+        const voter = await contract.voterRight(address, name, fileUrl, url, {
+          gasLimit: 1000000,
+        });
+        voter.wait();
+        console.log(`Voter Registered:✅ `);
+        console.log(
+          `voter data of address ${address} that will be redirected to voterlist:`,
+          voter
+        );
+        // voterAddress.push(address);
+        // console.log("Voters Address from create Voter:", voterAddress);
+        // navigate("/voterlist"); // navigate to the voter list page
+      } catch (error) {
+        console.log("Couldnt register:", error);
+      }
+      // navigate("/voterList");
+    } catch (error) {
+      setError("Error in creating voter", error);
+      return false;
+    }
+  };
+
+  //GET VOTER DATA ----TO BE DEBUGGED
+  const getAllVoterData = async (event) => {
+    //CONNECTING SMART CONTRACT
+    try {
+      const providerOptions = {};
+      const web3Modal = new Web3Modal({
+        providerOptions,
+      });
+      const connection = await web3Modal.connect();
+      //   // console.log("the following object is here:  ");
+      //   console.log(web3Modal);
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract = fetchContract(signer);
+      console.log("get all voter contract:", contract);
+
+      // get voter data
+
+      try {
+        const voterListData = await contract.getVoterList({
+          gasLimit: 1000000,
+        });
+        // voterListData.wait();
+
+        setVoterAddress(voterListData);
+        console.log("Voter list from getAllvoterdata:", voterAddress);
+        voterListData.map(async (eL) => {
+          const singleVoterData = await contract.getVoterData(eL);
+          pushCandidate.push(singleVoterData);
+          // console.log("single voter data:", singleVoterData);
+        });
+      } catch (error) {
+        console.log("Error while getting voter list:", error);
+      }
+      //VOTER LENGTH
+      const voterListLength = await contract.getVoterLength();
+      // console.log("voterListLengt:", voterListLength.toNumber());
+      setVoterLength(voterListLength.toNumber());
+    } catch (error) {
+      setError("Something went wron fetching data", error);
+    }
+  };
+  // console.log("voter Address OUTSIDE:", voterAddress);
+  useEffect(() => {
+    getAllVoterData();
+  }, []);
+
+  //to be debugged later
+  //ERROR: call revert exception [ See: https://links.ethers.org/v5-errors-CALL_EXCEPTION ] (method="getVoterList()", data="0x", errorArgs=null, errorName=null, errorSignature=null, reason=null, code=CALL_EXCEPTION, version=abi/5.7.0)
+  // at Logger.makeError (index.ts:269:1)
+  // at Logger.throwError (index.ts:281:1)
+  // at Interface.decodeFunctionResult (interface.ts:427:1)
+  // at Contract.<anonymous> (index.ts:400:1)
+  // at Generator.next (<anonymous>)
+  // at fulfilled (index.ts:1:1)
+
+  // console.log("calling functioon getAllVoterData:");
+  // useEffect(() => {
+  //   getAllVoterData();
+  // }, []);
 
   return (
     <VotingContext.Provider
@@ -76,6 +205,10 @@ export const VotingProvider = ({ children }) => {
         checkIfWalletIsConnected,
         connectWallet,
         uploadToIPFS,
+        createVoter,
+        getAllVoterData,
+        // createVoter,
+        // getAllVoterData,
       }}
     >
       {children}
--- frontend/src/context/constants.js
+++ frontend/src/context/constants.js
@@ -1,4 +1,4 @@
 //0x5FbDB2315678afecb367f032d93F642f64180aa3
 import voting from "./Create.json";
-export const votingAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
-export const votingAddressABI = voting.abi;
+export const VotingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const VotingAddressABI = voting.abi;
