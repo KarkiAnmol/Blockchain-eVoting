c983bfa9c4f827bcdc4595e6214bc8a5a254549c
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2023-05-01 16:43:42 +0545

    Refactored the content of Howitworks page and added some content to features page

--- frontend/src/pages/HowItWorks.js
+++ frontend/src/pages/HowItWorks.js
@@ -11,31 +11,6 @@ const HowItWorks = () => {
         candidates and monitor the voting process.
       </p>
 
-      <h2 className="how-it-works-page__subtitle">Features</h2>
-      <ul className="how-it-works-page__list">
-        <li className="how-it-works-page__list-item">
-          Voter verification: Voters must be verified using their unique ID
-          before being allowed to cast their votes.
-        </li>
-        <li className="how-it-works-page__list-item">
-          Candidate registration: Only the admin can create candidates and add
-          them to the candidate list.
-        </li>
-        <li className="how-it-works-page__list-item">
-          Secure voting: Voters can only vote once and their vote is stored on
-          the blockchain, making it tamper-proof and transparent.
-        </li>
-        <li className="how-it-works-page__list-item">
-          Winner determination: The winner of the election is determined
-          automatically after a fixed interval of time has passed since the
-          voting period ended.
-        </li>
-        <li className="how-it-works-page__list-item">
-          Voter anonymity: Voters' identities are kept anonymous on the
-          blockchain, ensuring the privacy of their vote.
-        </li>
-      </ul>
-
       <h2 className="how-it-works-page__subtitle">How to Use</h2>
       <ol className="how-it-works-page__list">
         <li className="how-it-works-page__list-item">
